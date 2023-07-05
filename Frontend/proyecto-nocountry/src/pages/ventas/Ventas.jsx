c808bf5c19e2bc13a98e4d11820f43c3935197e4
Anthony Donayre <e.a.donayre95@gmail.com>
2023-07-04 20:46:34 -0500

    prop container en ventas

--- Frontend/proyecto-nocountry/src/pages/ventas/Ventas.jsx
+++ Frontend/proyecto-nocountry/src/pages/ventas/Ventas.jsx
@@ -1,10 +1,11 @@
 import { Button } from "@mui/material";
 import Card from '../../components/Card/Card';
 import '../ventas/ventas-style.css'
+import { Container } from "react-bootstrap";
 
 const Ventas = () => {
   return (
-    <div className='ventas-main'>
+    <Container maxWidth="md">
       <div className="title">
         <h2>Lorem, ipsum dolor sit amet</h2>
       </div>
@@ -16,7 +17,7 @@ const Ventas = () => {
           description={"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa quaerat esse quo quae, earum officiis mollitia repellendus cupiditate dolorem sunt, iusto autem id. Consequuntur ipsum amet aliquam excepturi debitis modi."}
           img={'https://emprendedor.com/wp-content/uploads/2021/08/20190527193725-online.jpeg'} />
       </div>
-    </div>
+    </Container>
   )
 }
 
