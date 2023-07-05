2e04b89d531cb95cd1ba211e863a8dcde76f7aee
Anthony Donayre <e.a.donayre95@gmail.com>
2023-07-04 19:58:53 -0500

    se agrega pagina para producto unico

--- Frontend/proyecto-nocountry/src/App.jsx
+++ Frontend/proyecto-nocountry/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/home/Home.jsx";
 import Compras from "./pages/compras/Compras.jsx";
 import Login from "./pages/login/Login.jsx";
 import Ventas from "./pages/ventas/Ventas.jsx";
+import Producto from "./pages/producto/Producto.jsx";
 import Navbar from "./components/navbar/Navbar.jsx";
 import HomeIcon from "@mui/icons-material/Home";
 import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
@@ -30,6 +31,10 @@ const navLinks = [
     path: "/login",
     icon: <LoginIcon />,
   },
+  {
+    title: "Producto",
+    path: "/producto",
+  },
 ];
 
 const App = () => {
@@ -41,6 +46,7 @@ const App = () => {
         <Route path="/compras" element={<Compras />} />
         <Route path="/ventas" element={<Ventas />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/producto" element={<Producto />}/>
       </Routes>
     </>
   );
