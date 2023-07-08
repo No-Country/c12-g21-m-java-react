5f2f596163132654639c48251ec76155edb32074
npotaves <104572840+npotaves@users.noreply.github.com>
2023-07-07 23:33:36 -0300

    Revert "Test deploy11"

--- Backend/src/main/java/nocountry/ecommerce/config/MapperConfig.java
+++ Backend/src/main/java/nocountry/ecommerce/config/MapperConfig.java
@@ -1,14 +1,4 @@
 package nocountry.ecommerce.config;
 
-import org.modelmapper.ModelMapper;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
 public class MapperConfig {
-    @Bean("defaultMapper")
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
-
 }
--- Backend/src/main/java/nocountry/ecommerce/repositories/ICountryRepo.java
+++
@@ -1,6 +0,0 @@
-package nocountry.ecommerce.repositories;
-
-import nocountry.ecommerce.models.Country;
-
-public interface ICountryRepo extends IGenericRepo<Country, Integer> {
-}
