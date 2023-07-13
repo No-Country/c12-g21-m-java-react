6f2a0a99853255e432f6bd5093a70d40c3afae26
WebDevCod <pabloaquino1988@gmail.com>
2023-07-13 00:46:53 -0300

    modificacion de navLinks en App.jsx para cambiar la navbar segun se este login/logout

--- Frontend/proyecto-nocountry/src/pages/cerrar-sesion/CerrarSesion.jsx
+++ Frontend/proyecto-nocountry/src/pages/cerrar-sesion/CerrarSesion.jsx
@@ -1,20 +1,22 @@
-import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { cerrarSesion } from '../../features/userSlice'
-import { useNavigate } from 'react-router-dom'
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { cerrarSesion } from "../../features/userSlice";
+import { useNavigate } from "react-router-dom";
 
 const CerrarSesion = () => {
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
-    useEffect(() => {
-        setTimeout(() => {
-            dispatch(cerrarSesion())
-            navigate('/')
-        }, 2000)
-    })
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  useEffect(() => {
+    setTimeout(() => {
+      dispatch(cerrarSesion());
+      navigate("/");
+    }, 2000);
+  });
   return (
-      <div><h2>Cerrando sesion</h2></div>
-  )
-}
+    <div>
+      <h2>Cerrando sesion</h2>
+    </div>
+  );
+};
 
-export default CerrarSesion
+export default CerrarSesion;
