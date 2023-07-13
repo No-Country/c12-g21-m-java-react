971849dfd4e5dbd52161df9345d819d7d799db8a
Anthony Donayre Vilca <e.a.donayre95@gmail.com>
2023-07-13 14:51:29 -0500

    se dan opciones desplegables a los links de categoria y estado

--- Frontend/proyecto-nocountry/src/components/inputs/InputLink.jsx
+++ Frontend/proyecto-nocountry/src/components/inputs/InputLink.jsx
@@ -1,22 +1,61 @@
 import { Box, Link } from "@mui/material";
-
-const linkCat = <Link
-    component="button"
-    variant="body2"
-    onClick={() => {
-        console.info("Link categoria");
-    }}> Seleccione categoría </Link>
+import { useState } from "react";
 
 const InputLink = () => {
+    const [showOptions, setShowOptions] = useState(false);
+
+    const handleLinkClick = (event) => {
+        event.preventDefault(); // Evita la recarga de la página
+        setShowOptions(!showOptions);
+        console.info("Link categoria");
+    };
+
+    const linkCat = (
+        <Link
+            component="button"
+            variant="body2"
+            onClick={handleLinkClick}
+        >
+            Seleccione categoría
+        </Link>
+    );
+
+    //Funcion opciones
+    const option1 =
+        <Link>Opcion 1</Link>
+
+    const option2 =
+        <Link>Opcion 2</Link>
+
+    const option3 =
+        <Link>Opcion 3</Link>
+
     return (
-        <div id='inputLink'>
-            <Box component="form" sx={{ borderRadius: "none", border: ".4rem solid #d9d9d9", maxWidth: "90%", margin: "2rem auto", padding: ".3rem" }}>
+        <div id="inputLink">
+            <Box
+                component="form"
+                sx={{
+                    borderRadius: "none",
+                    border: ".4rem solid #d9d9d9",
+                    maxWidth: "90%",
+                    margin: "2rem auto",
+                    padding: ".3rem",
+                }}
+            >
                 <Box component="text">
                     {linkCat}
+                    {showOptions && (
+                        <div id="optionsLinks">
+                            {/* Opciones a mostrar */}
+                            <p>{option1}</p>
+                            <p>{option2}</p>
+                            <p>{option3}</p>
+                        </div>
+                    )}
                 </Box>
             </Box>
         </div>
-    )
-}
+    );
+};
 
-export default InputLink
+export default InputLink;
--- Frontend/proyecto-nocountry/src/components/inputs/InputLinkEstado.jsx
+++ Frontend/proyecto-nocountry/src/components/inputs/InputLinkEstado.jsx
@@ -1,22 +1,61 @@
 import { Box, Link } from "@mui/material";
-
-const linkEstado = <Link
-    component="button"
-    variant="body2"
-    onClick={() => {
-        console.info("Link estado de producto");
-    }}> Seleccione estado del producto </Link>
+import React, { useState } from "react";
 
 const InputLinkEstado = () => {
+    const [showOptions, setShowOptions] = useState(false);
+
+    const handleLinkClick = (event) => {
+        event.preventDefault(); // Evita la recarga de la página
+        setShowOptions(!showOptions);
+        console.info("Link estado de producto");
+    };
+
+    const linkEstado = (
+        <Link
+            component="button"
+            variant="body2"
+            onClick={handleLinkClick}
+        >
+            Seleccione estado del producto
+        </Link>
+    );
+
+    //Funcion opciones
+    const option1st =
+        <Link>Estado 1</Link>
+
+    const option2st =
+        <Link>Estado 2</Link>
+
+    const option3st =
+        <Link>Estado 3</Link>
+
     return (
         <div>
-            <Box component="form" sx={{ borderRadius: "none", border: ".4rem solid #d9d9d9", maxWidth: "90%", margin: "2rem auto", padding: ".3rem" }}>
+            <Box
+                component="form"
+                sx={{
+                    borderRadius: "none",
+                    border: ".4rem solid #d9d9d9",
+                    maxWidth: "90%",
+                    margin: "2rem auto",
+                    padding: ".3rem",
+                }}
+            >
                 <Box component="text">
                     {linkEstado}
+                    {showOptions && (
+                        <div id="optionsState">
+                            {/* Opciones a mostrar */}
+                            <p>{option1st}</p>
+                            <p>{option2st}</p>
+                            <p>{option3st}</p>
+                        </div>
+                    )}
                 </Box>
             </Box>
         </div>
-    )
-}
+    );
+};
 
-export default InputLinkEstado
+export default InputLinkEstado;
