d533574bb9ed0daa46269801fcd67aef4f07de18
WebDevCod <pabloaquino1988@gmail.com>
2023-07-18 18:47:12 -0300

    modificaciones en compras.jsx y productfilter.jsx para conectar con base de datos

--- Frontend/proyecto-nocountry/src/components/productFilter/ProductFilter.jsx
+++ Frontend/proyecto-nocountry/src/components/productFilter/ProductFilter.jsx
@@ -1,109 +1,132 @@
-import { FormGroup, FormControlLabel, Radio, RadioGroup } from '@mui/material';
+import { FormGroup, FormControlLabel, Radio, RadioGroup } from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-const ProductFilter = ({ options, setOptions }) => {
-    const handleOptionChange = (event) => {
-        const { name, value } = event.target;
-        setOptions((prevOptions) => ({
-            ...prevOptions,
-            [name]: value,
-        }));
-    };
+// eslint-disable-next-line react/prop-types
+const ProductFilter = ({ setOptions }) => {
+  const [selectedHouseRoom, setSelectedHouseRoom] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [selectedCondition, setSelectedCondition] = useState("");
 
-    return (
-        <div>
-            <h3 className="filtro-title">Filtrar por:</h3>
-            <FormGroup>
-                <h5>Categoría</h5>
-                <RadioGroup
-                    aria-label="category"
-                    name="category"
-                    value={options.category}
-                    onChange={handleOptionChange}
-                >
-                    <FormControlLabel
-                        value="Muebles"
-                        control={<Radio />}
-                        label="Muebles"
-                    />
-                    <FormControlLabel
-                        value="Accesorios"
-                        control={<Radio />}
-                        label="Accesorios"
-                    />
-                    <FormControlLabel
-                        value="Mesadas"
-                        control={<Radio />}
-                        label="Mesadas"
-                    />
-                    <FormControlLabel
-                        value=""
-                        control={<Radio />}
-                        label="Todos"
-                    />
-                </RadioGroup>
-            </FormGroup>
-            <h5>Ambiente</h5>
-            <FormGroup>
-                <RadioGroup
-                    aria-label="ambient"
-                    name="ambient"
-                    value={options.ambient}
-                    onChange={handleOptionChange}
-                >
-                    <FormControlLabel
-                        value="Living"
-                        control={<Radio />}
-                        label="Living"
-                    />
-                    <FormControlLabel
-                        value="Comedor"
-                        control={<Radio />}
-                        label="Comedor"
-                    />
-                    <FormControlLabel
-                        value="Dormitorio"
-                        control={<Radio />}
-                        label="Dormitorio"
-                    />
-                    <FormControlLabel
-                        value=""
-                        control={<Radio />}
-                        label="Todos"
-                    />
-                </RadioGroup>
-            </FormGroup>
-            <h5>Condición</h5>
-            <FormGroup>
-                <RadioGroup
-                    aria-label="condition"
-                    name="condition"
-                    value={options.condition}
-                    onChange={handleOptionChange}
-                >
-                    <FormControlLabel
-                        value="Buena"
-                        control={<Radio />}
-                        label="Buena"
-                    />
-                    <FormControlLabel
-                        value="Muy buena"
-                        control={<Radio />}
-                        label="Muy buena"
-                    />
-                    <FormControlLabel
-                        value="Excelente"
-                        control={<Radio />}
-                        label="Excelente"
-                    />
-                    <FormControlLabel
-                        value=""
-                        control={<Radio />}
-                        label="Todos"
-                    />
-                </RadioGroup>
-            </FormGroup>
-        </div>
-    );
+  const handleHouseRoomChange = (event) => {
+    setSelectedHouseRoom(event.target.value);
+  };
+
+  const handleCategoryChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  const handleConditionChange = (event) => {
+    setSelectedCondition(event.target.value);
+  };
+
+  const [categoryStatusList, setCategoryStatusList] = useState([]);
+  const [categoryProductList, setCategoryProductList] = useState([]);
+  const [categoryHouseRoomsList, setCategoryHouseRoomsList] = useState([]);
+
+  useEffect(() => {
+    const statusUrl =
+      "https://c12-21-m-java-react-ecommerce.onrender.com/categoryStatus/list";
+    const productUrl =
+      "https://c12-21-m-java-react-ecommerce.onrender.com/categoryProduct/list";
+    const roomsUrl =
+      "https://c12-21-m-java-react-ecommerce.onrender.com/categoryHouseRooms/list";
+
+    axios
+      .all([axios.get(statusUrl), axios.get(productUrl), axios.get(roomsUrl)])
+      .then(
+        axios.spread((statusResponse, productResponse, roomsResponse) => {
+          const statusData = statusResponse.data;
+          const productData = productResponse.data;
+          const roomsData = roomsResponse.data;
+
+          setCategoryStatusList(statusData);
+          setCategoryProductList(productData);
+          setCategoryHouseRoomsList(roomsData);
+        })
+      )
+      .catch((error) => {
+        console.error("Error al hacer la solicitud:", error);
+      });
+  }, []);
+
+  useEffect(() => {
+    setOptions((prevOptions) => ({
+      ...prevOptions,
+      houseRoom: selectedHouseRoom,
+      category: selectedCategory,
+      condition: selectedCondition,
+    }));
+  }, [selectedHouseRoom, selectedCategory, selectedCondition, setOptions]);
+
+  return (
+    <div>
+      <h3 className="filtro-title">Filtrar por:</h3>
+
+      {/* houseRoom */}
+      <h5>Habitación</h5>
+      <FormGroup>
+        <RadioGroup
+          aria-label="house-room"
+          name="houseRoom"
+          value={selectedHouseRoom}
+          onChange={handleHouseRoomChange}
+        >
+          {categoryHouseRoomsList.map((categoryHouseRoom) => (
+            <FormControlLabel
+              key={categoryHouseRoom.idCategoryHouseRooms}
+              value={categoryHouseRoom.idCategoryHouseRooms}
+              control={<Radio />}
+              label={categoryHouseRoom.title}
+            />
+          ))}
+          <FormControlLabel value="" control={<Radio />} label="Todos" />
+        </RadioGroup>
+      </FormGroup>
+
+      {/* category */}
+      <h5>Categoría</h5>
+      <FormGroup>
+        <RadioGroup
+          aria-label="category"
+          name="category"
+          value={selectedCategory}
+          onChange={handleCategoryChange}
+        >
+          {categoryProductList.map((categoryProduct) => (
+            <FormControlLabel
+              key={categoryProduct.idCategoryProduct}
+              value={categoryProduct.idCategoryProduct}
+              control={<Radio />}
+              label={categoryProduct.title}
+            />
+          ))}
+          <FormControlLabel value="" control={<Radio />} label="Todos" />
+        </RadioGroup>
+      </FormGroup>
+
+      {/* condition */}
+      <h5>Condición</h5>
+      <FormGroup>
+        <RadioGroup
+          aria-label="condition"
+          name="condition"
+          value={selectedCondition}
+          onChange={handleConditionChange}
+        >
+          {categoryStatusList.map((category) => (
+            <FormControlLabel
+              key={category.idCategoryStatus}
+              value={category.idCategoryStatus}
+              control={<Radio />}
+              label={category.title}
+            />
+          ))}
+          <FormControlLabel value="" control={<Radio />} label="Todos" />
+        </RadioGroup>
+      </FormGroup>
+    </div>
+  );
 };
 
 export default ProductFilter;
